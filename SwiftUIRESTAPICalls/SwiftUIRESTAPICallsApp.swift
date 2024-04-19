becd0ae7fded3c28af21d331b20e2c1cb6c6350c
gabriel850129 <gabriel850129@icloud.com>
2024-04-19 13:56:57 -0400

    Initial Commit

---
+++ SwiftUIRESTAPICalls/SwiftUIRESTAPICallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRESTAPICallsApp.swift
+//  SwiftUIRESTAPICalls
+//
+//  Created by Gabriel Estévez López on 4/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRESTAPICallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
